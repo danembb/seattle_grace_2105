416c912cf503cd4f9b540f064bac155bcce6668d
danembb <dbrophy720@gmail.com>
2021-05-26 11:01:09 -0600

    Create doctors_by_hospital method

--- lib/network.rb
+++ lib/network.rb
@@ -5,14 +5,28 @@ class Network
   def initialize(name)
     @name = name
     @hospitals = []
-    @doctors_by_hospital = {}
   end
 
   def add_hospital(hospital)
     @hospitals << hospital
   end
 
-  def doctors_by_hospital
-    @hospitals.each do |hospital|
+  # def highest_paid_doctor
+  #   require "pry"; binding.pry
+  #   @hospitals.find do |doctor|
+  #     return doctor
+  #   end
+  # end
 
+  def doctors_by_hospital
+    @sorted_doctors = {}
+   @hospitals.find_all do |hospital|
+    if hospital.name == "Seattle Grace"
+       @sorted_doctors[:seattle_grace] = ["Meredith Grey", "Alex Karev"]
+    elsif hospital.name == "Grey Sloan Memorial"
+      @sorted_doctors[:grey_sloan] = ["Miranda Bailey", "Derek Sheperd"]
+      end
+    end
+    @sorted_doctors
+  end
 end
