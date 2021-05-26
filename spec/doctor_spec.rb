af4aa530da325af14fff578b301768a1e2bcce8d
danembb <dbrophy720@gmail.com>
2021-05-26 09:09:52 -0600

    Test attributes for doctor class

--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -8,10 +8,13 @@ RSpec.describe Doctor do
       expect(meredith).to be_an_instance_of(Doctor)
     end
 
-    # it "has attributes" do
-    #   meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
-    #
-    #   expect(meredith.name)
-    # end
+    it "has attributes" do
+      meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+
+      expect(meredith.name).to eq("Meredith Grey")
+      expect(meredith.specialty).to eq("General Surgery")
+      expect(meredith.education).to eq("Harvard University")
+      expect(meredith.salary).to eq(100_000)
+    end
   end
 end
