79cf8a3611188b1a8dca1a8dc98ff024bd257800
danembb <dbrophy720@gmail.com>
2021-05-26 10:05:57 -0600

    Create total_salary method for hospital class

--- lib/hospital.rb
+++ lib/hospital.rb
@@ -8,4 +8,18 @@ class Hospital
     @chief_of_surgery = chief_of_surgery
     @doctors = doctors
   end
+
+  def total_salary
+    @salaries = []
+    @doctors.each do |doctor|
+      @salaries << doctor.salary
+    end
+    @salaries.sum
+  end
+
+  # def lowest_paid_doctor
+  #   @doctors.each do |doctor|
+  #     doctor.salary.min
+  #   end
+  # end
 end
